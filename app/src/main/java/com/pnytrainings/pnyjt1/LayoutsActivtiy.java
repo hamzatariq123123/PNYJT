7327c8550579e7d8224c2e9d37cc555329b217a4
hamza tariq <hamza.tariq@synavos.com>
2020-02-26 20:48:20 +0500

    layouts lecture

---
+++ app/src/main/java/com/pnytrainings/pnyjt1/LayoutsActivtiy.java
@@ -0,0 +1,14 @@
+package com.pnytrainings.pnyjt1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LayoutsActivtiy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear_layout);
+    }
+}
